ae851cb4f7e5303f36e73c99d7bda5c3b91a2661
Tarun Aggarwal <tarunaggarwal441@gmail.com>
2024-01-27 16:50:09 +0530

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,15 @@
+package com.kapil.learningjava.basics;
+
+/**
+ * First Java program.
+ *
+ * @author Kapil Garg
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, World. I'm Kapil Garg.\n");    // \n for the new line
+        System.out.println("This is my first Java program.");   // println method will take care of the new line
+    }
+
+}
