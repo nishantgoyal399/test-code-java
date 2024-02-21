6d4053c4231c30460b8cb4a16cf07f059a5dcb76
Tarun Aggarwal <tarunaggarwal441@gmail.com>
2024-02-21 22:38:58 +0530

    Add files via upload

--- PrePostDecrementDemo.java
+++ PrePostDecrementDemo.java
@@ -1,7 +1,7 @@
-package com.kapil.learningjava.operators;
+package com.kapil.learnjava.operators;
 
 /**
- * Program to understand Pre Post Decrement operator in Java.
+ * Program to understand Pre-Post Decrement Operator in Java.
  *
  * @author Kapil Garg
  */
@@ -10,17 +10,17 @@ public class PrePostDecrementDemo {
     public static void main(String[] args) {
         int i = 4;
         int j = i--;
-        System.out.println("Value of i = " + i);    // Output 3
-        System.out.println("Value of j = " + j);    // Output 4
+        System.out.println("Value of i :: " + i);    // Output 3
+        System.out.println("Value of j :: " + j);    // Output 4
         int k = --i;
-        System.out.println("Value of i = " + i);    // Output 2
-        System.out.println("Value of k = " + k);    // Output 2
+        System.out.println("Value of i :: " + i);    // Output 2
+        System.out.println("Value of k :: " + k);    // Output 2
         j = i--;
-        System.out.println("Value of i = " + i);    // Output 1
-        System.out.println("Value of j = " + j);    // Output 2
+        System.out.println("Value of i :: " + i);    // Output 1
+        System.out.println("Value of j :: " + j);    // Output 2
         k = --i;
-        System.out.println("Value of i = " + i);    // Output 0
-        System.out.println("Value of k = " + k);    // Output 0
+        System.out.println("Value of i :: " + i);    // Output 0
+        System.out.println("Value of k :: " + k);    // Output 0
     }
 
 }
--- PrePostIncrementDemo.java
+++ PrePostIncrementDemo.java
@@ -1,7 +1,7 @@
-package com.kapil.learningjava.operators;
+package com.kapil.learnjava.operators;
 
 /**
- * Program to understand Pre Post Increment operator in Java.
+ * Program to understand Pre-Post Increment Operator in Java.
  *
  * @author Kapil Garg
  */
@@ -10,17 +10,17 @@ public class PrePostIncrementDemo {
     public static void main(String[] args) {
         int i = 4;
         int j = i++;
-        System.out.println("Value of i = " + i);    // Output 5
-        System.out.println("Value of j = " + j);    // Output 4
+        System.out.println("Value of i :: " + i);    // Output 5
+        System.out.println("Value of j :: " + j);    // Output 4
         int k = ++i;
-        System.out.println("Value of i = " + i);    // Output 6
-        System.out.println("Value of k = " + k);    // Output 6
+        System.out.println("Value of i :: " + i);    // Output 6
+        System.out.println("Value of k :: " + k);    // Output 6
         j = i++;
-        System.out.println("Value of i = " + i);    // Output 7
-        System.out.println("Value of j = " + j);    // Output 6
+        System.out.println("Value of i :: " + i);    // Output 7
+        System.out.println("Value of j :: " + j);    // Output 6
         k = ++i;
-        System.out.println("Value of i = " + i);    // Output 8
-        System.out.println("Value of k = " + k);    // Output 8
+        System.out.println("Value of i :: " + i);    // Output 8
+        System.out.println("Value of k :: " + k);    // Output 8
     }
 
 }
