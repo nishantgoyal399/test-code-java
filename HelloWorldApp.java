67232b8a2f37433298bbd05b455f39b729336774
Nishant Goyal <104673360+nishantgoyal399@users.noreply.github.com>
2024-12-23 22:22:14 +0530

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,15 @@
+package com.kapil.java.basics;
+
+/**
+ * First Java Program.
+ *
+ * @author Kapil Garg
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, World. I'm Kapil Garg.\n");		        // \n for the new line
+        System.out.println("Welcome to the world of Java programming.");	// println automatically adds a new line
+    }
+
+}
