42db19a445664eda798be833cdf807658835aa89
Nishant Goyal <104673360+nishantgoyal399@users.noreply.github.com>
2025-01-11 18:43:43 +0530

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,16 @@
+package com.kapil.java.basics;
+
+/**
+ * First Java Program that prints a welcome message to the console.
+ *
+ * @author Kapil Garg
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, World!\n");                                // \n for the new line
+        System.out.println("I'm Kapil Garg, ready to explore Java!");       // println automatically adds a new line
+        System.out.println("Ready to explore the world of Java, one program at a time!");
+    }
+
+}
