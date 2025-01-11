b19c534ceabb551b0b1c52de10c278e5aae05f2c
Nishant Goyal <104673360+nishantgoyal399@users.noreply.github.com>
2025-01-11 18:45:02 +0530

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -10,7 +10,7 @@ public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.print("Hello, World!\n");                                // \n for the new line
         System.out.println("I'm Kapil Garg, ready to explore Java!");       // println automatically adds a new line
-        System.out.println("Ready to explore the world of Java, one program at a time!");
+        System.out.println("This is the beginning of a long journey into programming!");
     }
 
 }
